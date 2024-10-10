eab82cb33f4e3f05af0330d4503a566d1aad0199
Ashish <s8asranj@stud.uni-saarland.de>
2024-10-10 19:59:15 +0200

    DAY 3: Signal Added as another method for state management

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,11 +11,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //We made selectedUser now a signal that we invoke as a function at other places.  
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
   //Here I make a 'getter' path for the image.
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.selectedUser().avatar;
   }
 
   onSelectUser() {
@@ -24,6 +25,6 @@ export class UserComponent {
     // I change the current user to some other random user 
     // Then I display this new random user. 
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]; // Update the selected user
+    this.selectedUser = signal(DUMMY_USERS[randomIndex]); // Update the selected user
     }
 }
