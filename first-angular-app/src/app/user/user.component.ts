c312725d8eb66ee0fd0abebe254a2a5280db75c0
Ashish <s8asranj@stud.uni-saarland.de>
2024-10-12 13:51:50 +0200

    DAY 4: Added @Input() to make the components configurable and more reusable.

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,22 +9,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  //We made selectedUser now a signal that we invoke as a function at other places.  
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  //Now we use 'computed' that uses signal inside whenever any signal gets a change.
-  imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar )
-  
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return '/assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
     
-    // 1.  When user clicks on the button, it should change the user 
-    // I change the current user to some other random user 
-    // Then I display this new random user. 
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]); // Update the selected user
-    }
+  }
 }
